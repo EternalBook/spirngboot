23037a5afb20844da022049f2576f924ca45d80f
EternalBook <2435320866@qq.com>
2023-03-20 11:19:27 +0800

    1.0

---
+++ src/main/java/com/dvctt/yt/SpringbokApplication.java
@@ -0,0 +1,16 @@
+package com.dvctt.yt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author etern
+ */
+@SpringBootApplication
+public class SpringbokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbokApplication.class, args);
+    }
+
+}
